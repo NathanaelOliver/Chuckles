459ca697b7809a5b0e08d8565eecc27b4ed65fc9
NathanaelOliver <nathanaeloliver437@gmail.com>
2021-06-02 15:08:57 -0400

    Added multiple rows

--- MakeHTML.py
+++ MakeHTML.py
@@ -2,31 +2,58 @@ import os
 
 def main():
   f = open("scrollbar.html","w+")
+  fabuwood_arr = sort(os.listdir('./Cabinets/Fabuwood/'))
+  cnc_arr = sort(os.listdir('./Cabinets/CNC/'))
   print_begin(f)
   print_head(f)
-  print_style(f)
+  f.write("  <style>\n")
+  print_style(f, "Fabuwood", fabuwood_arr)
+  print_style(f, "CNC", cnc_arr)
+  f.write("  </style>\n")
   print_scripts(f)
-  print_body(f)
+  f.write("  <body>\n")
+  print_body(f, "Fabuwood", fabuwood_arr)
+  print_body(f, "CNC", cnc_arr)
+  f.write("  </body>\n")
   print_end(f)
 
 def print_begin(f):
   f.write("<!DOCTYPE html>\n<html lang=\"en\">\n")
 
 def print_head(f):
-  f.write("\t<head>\n\t\t<title>Scrollbar Widget</title>\n\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t</head>\n")
+  f.write("  <head>\n    <title>Scrollbar Widget</title>\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n  </head>\n")
 
+def print_style(f, company, arr):
+  
+  f.write("    :root {\n      --black: #000000;\n      --beige: #d4c9bd;\n      --white: #ffffff;\n      --brown: #5a4d46;\n      --green: #9ba167;\n      --none: #ffffff00;\n    }\n")
+  f.write("    div.scrollmenu {\n      background-color: var(--beige);\n      overflow: auto;\n      white-space: nowrap;\n      display: flex;\n    }\n")
+  f.write("    div.card {\n      min-width: 250px;\n      display: block;\n      padding: 20px;\n      padding-left: 10px;\n      padding-right: 10px;\n      margin: 20px;\n      margin-bottom: 10px;\n      background: var(--white);\n      border-radius: 10px;\n      border-style: solid;\n      border-width: 4px;\n      border-color: var(--green);\n    }\n")
+  f.write("    div.card h1{\n      text-align: center;\n      color: var(--brown);\n    }\n")
+  f.write("    div.card img{\n      height: 300px;\n      display: block;\n      margin-left: auto;\n      margin-right: auto;\n      margin-bottom: 20px;\n    }\n")
+  f.write("    div.button-group {\n      border-radius: 1px;\n      border-color: var(--black);\n      margin: auto;\n      display: flex;\n      flex-direction: row;\n      justify-content: center;\n    }\n")
+  f.write("    div.button-group button{\n      color: var(--none);\n      width: 30px;\n      height: 52px;\n      cursor: pointer;\n    }\n")
+  create_custom_buttons(f, company, arr)
 
+def create_custom_buttons(f, company, arr):
+  for element in arr:
+    for e in element:
+      f.write("    button.btn-")
+      f.write(unformat(e))
+      f.write(" {\n      background-image:url('Cabinets/" + company + "/")
+      f.write(e)
+      f.write("');\n      background-size: 26px;\n      margin: 2px;\n    }\n")
+
+def unformat(str):
+  return str.replace(" ", "-")[0:-4]
 
 def print_scripts(f):
-  f.write("\t<script>\n\t\tfunction changeImage(id, newImage) {\n\t\t\tvar img = document.getElementById(id);\n\t\t\timg.src=newImage;\n\t\t\treturn false;\n\t\t}\n\t</script>\n")
+  f.write("  <script>\n    function changeImage(id, new_image, new_text) {\n      var img = document.getElementById(id + \" img\");\n      img.src=new_image;\n      document.getElementById(id + \" title\").innerHTML = new_text;\n      return false;\n    }\n  </script>\n")
 
-def print_body(f):
-  f.write("\t<body>\n\t\t<div class=\"scrollmenu\">\n")
-  arr = os.listdir('./Cabinets')
-  sorted_arr = sort(arr)
-  for element in sorted_arr:
-    make_card(f, element)
-  f.write("\t\t</div>\n\t</body>\n")
+def print_body(f, company, arr):
+  f.write("    <div class=\"scrollmenu\">\n")
+  for element in arr:
+    make_card(f, company, element)
+  f.write("    </div>\n")
 
 def sort(arr):
   list1 = []
@@ -40,25 +67,25 @@ def sort(arr):
     list2.append(element)
   return list1
 
-def make_card(f, set):
-  f.write("\t\t\t<div class=\"card\">\n\t\t\t\t<h1>")
+def make_card(f, company, set):
+  f.write("      <div class=\"card\">\n        <h1 id=\"" + company + " " + type_of(set[0]) + " title" + "\">")
   f.write(format(set[0]))
-  f.write("</h1>\n\t\t\t\t<img id=\"")
-  f.write(type_of(set[0]))
-  f.write("\" src=\"Cabinets/")
-  f.write(set[0])
+  f.write("</h1>\n        <img id=\"")
+  f.write(company + " " + type_of(set[0]) + " img")
+  f.write("\" src=\"Cabinets/" + company + '/' + set[0])
   f.write("\" alt=\"Image Unavailable\">\n")
-  f.write("\t\t\t\t<div class=\"button-group\">\n")
+  f.write("        <div class=\"button-group\">\n")
   for element in set:
-    make_button(f, element)
-  f.write("\t\t\t\t</div>\n")
-  f.write("\t\t\t</div>\n")
+    make_button(f, company, element)
+  f.write("        </div>\n")
+  f.write("      </div>\n")
 
-def make_button(f, element):
-  f.write("\t\t\t\t\t<button onclick=\"changeImage('")
-  f.write(type_of(element))
-  f.write("', 'Cabinets/")
-  f.write(element)
+def make_button(f, company, element):
+  f.write("          <button class=\"btn-")
+  f.write(unformat(element))
+  f.write("\" onclick=\"changeImage('")
+  f.write(company + " " + type_of(element))
+  f.write("', 'Cabinets/" + company + "/" + element + "', '" + format(element))
   f.write("');\"/>\n")
 
 def format(str):
@@ -69,82 +96,5 @@ def type_of(str):
 
 def print_end(f):
   f.write("</html>\n")
-
-
-def print_style(f):
-  f.write('''<style>
-:root {
---beige: #d4c9bd;
---white: #ffffff;
---brown: #5a4d46;
---green: #9ba167;
---none: #ffffff00;
-}
-div.scrollmenu {
-background-color: var(--beige);
-overflow: auto;
-white-space: nowrap;
-display: flex;
-}
-div.card {
-padding: 20px;
-padding-left: 30px;
-padding-right: 30px;
-margin: 40px;
-margin-bottom: 10px;
-background: var(--white);
-border-radius: 10px;
-border-style: solid;
-border-width: 4px;
-border-color: var(--green);
-}
-div.card h1{
-text-align: center;
-color: var(--brown);
-}
-div.card img{
-height: 400px;
-display: block;
-margin-left: auto;
-margin-right: auto;
-margin-bottom: 20px;
-}
-div.button-group {
-margin: auto;
-display: flex;
-flex-direction: row;
-justify-content: center;
-}
-div.button-group button{
-background-color: var(--green); /* Green background */
-border: 1px solid var(--brown); /* Green border */
-color: var(--none); /* White text */
-width: 30px;
-height: 30px;
-cursor: pointer; /* Pointer/hand icon */
-}
-</style>''')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 main()
